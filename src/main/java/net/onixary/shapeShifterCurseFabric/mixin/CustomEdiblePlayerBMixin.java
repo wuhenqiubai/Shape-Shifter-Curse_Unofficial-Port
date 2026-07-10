5df68af9bdedf013c5c47a4c1c0eadbbee6672d4
A Cat <wth110628@163.com>
2026-07-10 09:39:21 +0800

    饥饿值恢复修的大差不差了 饱食度仍然有问题

--- src/main/java/net/onixary/shapeShifterCurseFabric/mixin/CustomEdiblePlayerBMixin.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/mixin/CustomEdiblePlayerBMixin.java
@@ -2,12 +2,12 @@ package net.onixary.shapeShifterCurseFabric.mixin;
 
 import io.github.apace100.apoli.component.PowerHolderComponent;
 import io.github.apace100.apoli.power.ModifyFoodPower;
-import io.github.apace100.apoli.util.modifier.Modifier;
 import io.github.apace100.apoli.util.modifier.ModifierUtil;
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -15,10 +15,12 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
+import net.onixary.shapeShifterCurseFabric.ShapeShifterCurseFabric;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -53,26 +55,29 @@ public abstract class CustomEdiblePlayerBMixin extends LivingEntity {
     private FoodComponent ssc$modifyFoodComponent(FoodComponent original, World world, ItemStack stack) {
         PlayerEntity player = (PlayerEntity) (Object) this;
 
-        List<ModifyFoodPower> powers = PowerHolderComponent.getPowers(player, ModifyFoodPower.class)
-                .stream()
-                .filter(p -> p.doesApply(stack))
+        List<ModifyFoodPower> allPowers = PowerHolderComponent.getPowers(player, ModifyFoodPower.class);
+        ShapeShifterCurseFabric.LOGGER.info("[SSC] HEAD: powers={}, food={}, orig Nutr={}, Sat={}",
+                allPowers.size(), stack.getItem(), original.nutrition(), original.saturation());
+
+        List<ModifyFoodPower> powers = allPowers.stream()
+                .filter(p -> {
+                    boolean apply = p.doesApply(stack);
+                    ShapeShifterCurseFabric.LOGGER.info("[SSC] HEAD: doesApply {} = {}", p.getType().getIdentifier(), apply);
+                    return apply;
+                })
                 .toList();
 
-        if (powers.isEmpty()) return original;
-
-        List<Modifier> nutritionModifiers = powers.stream()
-                .flatMap(p -> p.getFoodModifiers().stream())
-                .toList();
-        List<Modifier> saturationModifiers = powers.stream()
-                .flatMap(p -> p.getSaturationModifiers().stream())
-                .toList();
-
-        int newNutrition = (int) ModifierUtil.applyModifiers(player, nutritionModifiers, original.nutrition());
-        float newSaturation = (float) ModifierUtil.applyModifiers(player, saturationModifiers, original.saturation());
-
-        if (newNutrition == original.nutrition() && newSaturation == original.saturation()) {
+        if (powers.isEmpty()) {
+            ShapeShifterCurseFabric.LOGGER.info("[SSC] HEAD: no powers, returning original");
             return original;
         }
+
+        int newNutrition = (int) ModifierUtil.applyModifiers(player,
+                powers.stream().flatMap(p -> p.getFoodModifiers().stream()).toList(), original.nutrition());
+        float newSaturation = (float) ModifierUtil.applyModifiers(player,
+                powers.stream().flatMap(p -> p.getSaturationModifiers().stream()).toList(), original.saturation());
+
+        ShapeShifterCurseFabric.LOGGER.info("[SSC] HEAD: NEW FoodComponent n={} s={}", newNutrition, newSaturation);
 
         return new FoodComponent(
                 newNutrition,
@@ -82,5 +87,19 @@ public abstract class CustomEdiblePlayerBMixin extends LivingEntity {
                 original.usingConvertsTo(),
                 original.effects()
         );
+    }
+
+    @Redirect(method = "eatFood", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;eat(Lnet/minecraft/component/type/FoodComponent;)V"))
+    private void ssc$redirectEat(HungerManager manager, FoodComponent foodComponent) {
+        ShapeShifterCurseFabric.LOGGER.info("[SSC] INVOKE: received n={}, s={}", foodComponent.nutrition(), foodComponent.saturation());
+        manager.eat(foodComponent);
+        int fl = manager.getFoodLevel();
+        float sl = manager.getSaturationLevel();
+        ShapeShifterCurseFabric.LOGGER.info("[SSC] INVOKE: after eat, foodLevel={}, satLevel={}", fl, sl);
+        if ((Object)this instanceof ServerPlayerEntity serverPlayer) {
+            serverPlayer.networkHandler.sendPacket(
+                    new net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket(
+                            serverPlayer.getHealth(), fl, sl));
+        }
     }
 }
