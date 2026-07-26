2a77bf7e3c434cd16412cc79bf5259561caf50ab
A Cat <wth110628@163.com>
2026-07-26 13:43:48 +0800

    迁移到MojMap

--- src/main/java/net/onixary/shapeShifterCurseFabric/mixin/ai/FleeEntityGoalCatFriendlyMixin.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/mixin/ai/FleeEntityGoalCatFriendlyMixin.java
@@ -1,29 +1,30 @@
 package net.onixary.shapeShifterCurseFabric.mixin.ai;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.goal.FleeEntityGoal;
-import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.entity.passive.CatEntity;
-import net.minecraft.entity.passive.OcelotEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.entity.animal.Ocelot;
+import net.minecraft.world.entity.player.Player;
 import net.onixary.shapeShifterCurseFabric.additional_power.AdditionalPowers;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(FleeEntityGoal.class)
+@Mixin(AvoidEntityGoal.class)
 public abstract class FleeEntityGoalCatFriendlyMixin<T extends LivingEntity> {
-    @Shadow @Final protected PathAwareEntity mob;
-    @Shadow @Nullable protected T targetEntity;
+    @Shadow @Final protected PathfinderMob mob;
+    @Unique @Nullable protected T target;
 
-    @Inject(method = "canStart", at = @At("RETURN"), cancellable = true)
-    private void ssc$modifyCanStart(CallbackInfoReturnable<Boolean> cir) {
-        if (cir.getReturnValue() && this.targetEntity instanceof PlayerEntity player) {
-            if ((this.mob instanceof OcelotEntity || this.mob instanceof CatEntity)
+    @Inject(method = "canUse", at = @At("RETURN"), cancellable = true)
+    private void ssc$modifycanUse(CallbackInfoReturnable<Boolean> cir) {
+        if (cir.getReturnValue() && this.target instanceof Player player) {
+            if ((this.mob instanceof Ocelot || this.mob instanceof Cat)
                 && AdditionalPowers.CAT_FRIENDLY.isActive(player)) {
                 cir.setReturnValue(false);
             }
