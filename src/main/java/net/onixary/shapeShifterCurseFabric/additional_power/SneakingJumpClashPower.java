e8e9ab10f092e9289a9c265231d4e27217e98c6f
A Cat <wth110628@163.com>
2026-08-27 19:59:23 +0800

    暂时修复issue #20 的问题，然后增加一些镜像

--- src/main/java/net/onixary/shapeShifterCurseFabric/additional_power/SneakingJumpClashPower.java
+++ src/main/java/net/onixary/shapeShifterCurseFabric/additional_power/SneakingJumpClashPower.java
@@ -11,7 +11,9 @@ import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.onixary.shapeShifterCurseFabric.ShapeShifterCurseFabric;
 
@@ -92,7 +94,7 @@ public class SneakingJumpClashPower extends Power {
                 LivingEntity.class, expandedBox, 
                 e -> e != player && e.isAlive() && !e.isRemoved())) {
 
-            if (player.getWorld().raycast(new RaycastContext(new Vec3d(player.getX(), player.getBodyY(0.5f), player.getZ()), new Vec3d(target.getX(), target.getBodyY(0.5f), target.getZ()), RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, player)).getType() == HitResult.Type.BLOCK) {
+            if (player.level().clip(new ClipContext(new Vec3(player.getX(), player.getY(0.5f), player.getZ()), new Vec3(target.getX(), target.getY(0.5f), target.getZ()), ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player)).getType() == HitResult.Type.BLOCK) {
                 continue;
             }
 
